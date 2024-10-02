4c41cc93b393065cc7d2d7c5686b8e5521918da9
Stephaneshb <stephane.shalaby@epitech.digital>
2024-10-02 15:30:36 +0200

    Create  ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,9 @@
+function capitalize(mots) {
+    return mots.map(mot => mot.toUpperCase() + ' ');
+}
+console.log(capitalize(['CapITAlize', 'tHis', 'text'])); 
+
+
+
+const result = capitalize( ["CHANGE", "this", "array", "BY", "ThE", "RESULT", "oF", "youR", "FUNCTION", "TO", "test", "IT", "OUT"])
+displayResult(result)
