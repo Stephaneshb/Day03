b4344b931c7bd1664db47ab65352db441f439b72
Stephaneshb <stephane.shalaby@epitech.digital>
2024-10-02 15:03:32 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,15 @@
+function getHousesPrototype() {
+    
+    let housesMap = new Map();
+
+ 
+    housesMap.set('Apartment 75008', 90);
+    housesMap.set('Apartment 94300', 105);
+    housesMap.set('Apartment 92300', 140);
+    housesMap.set('Apartment 75015', 65);
+
+    return housesMap;
+  }
+
+  
+  console.log(getHousesPrototype());
